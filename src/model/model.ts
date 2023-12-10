46b9b4b63e28910a156c2aa0464c2506d3a15107
tatianavega <tatiana.vega1@hotmail.com>
2023-12-10 15:48:10 -0300

    Incorporacion de la funcion getHourlyTemperatureAndHumidity

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,178 +1,33 @@
-import jsonfile from "jsonfile";
-import { pathFile } from "../database";
+import { readFileSync } from "node:fs";
+import { pathFile } from "../database/index";
+import * as interfaces from '../utils/interfaces';
 
-interface Weather {
-    timelines: DataMinutely[] | DataHourly[] | DataDaily[],
-    location: Location
+const getDataWeather = (): interfaces.Weather | false => {
+  const bufferData: Buffer = readFileSync(pathFile);
+  const parsedData: interfaces.Weather = JSON.parse(bufferData.toString());
+  return parsedData;
+};
+
+function getHourlyTemperatureAndHumidity(): { time: string, humidity: number, temperature: number }[] | false {
+  const weatherData: interfaces.Weather | false = getDataWeather();
+  if (!weatherData || !weatherData.timelines) {
+    return false;
+  }
+
+  let hourlyTimelines: interfaces.DataHourly[] | undefined;
+
+  if ('hourly' in weatherData.timelines) {
+    hourlyTimelines = weatherData.timelines.hourly as interfaces.DataHourly[];
+  } else {
+    return false;
+  }
+
+  return hourlyTimelines.map(timeline => ({
+    time: timeline.time,
+    humidity: timeline.values.humidity,
+    temperature: timeline.values.temperature
+  }));
 }
 
-interface ValuesMinutely {
-    "cloudBase": number,
-    "cloudCeiling": number | null
-    "cloudCover": number,
-    "dewPoint": number,
-    "freezingRainIntensity": number,
-    "humidity": number,
-    "precipitationProbability": number,
-    "pressureSurfaceLevel": number,
-    "rainIntensity": number,
-    "sleetIntensity": number,
-    "snowIntensity": number,
-    "temperature": number,
-    "temperatureApparent": number,
-    "uvHealthConcern": number,
-    "uvIndex": number,
-    "visibility": number,
-    "weatherCode": number,
-    "windDirection": number,
-    "windGust": number,
-    "windSpeed": number
-}
-
-interface DataHourly {
-    "cloudBase": number | null,
-    "cloudCeiling": number | null,
-    "cloudCover": number,
-    "dewPoint": number,
-    "evapotranspiration": number,
-    "freezingRainIntensity": number,
-    "humidity": number,
-    "iceAccumulation": number,
-    "iceAccumulationLwe": number,
-    "precipitationProbability": number,
-    "pressureSurfaceLevel": number,
-    "rainAccumulation": number,
-    "rainAccumulationLwe": number,
-    "rainIntensity": number,
-    "sleetAccumulation": number,
-    "sleetAccumulationLwe": number,
-    "sleetIntensity": number,
-    "snowAccumulation": number,
-    "snowAccumulationLwe": number,
-    "snowDepth": number,
-    "snowIntensity": number,
-    "temperature": number,
-    "temperatureApparent": number,
-    "uvHealthConcern": number,
-    "uvIndex": number,
-    "visibility": number,
-    "weatherCode": number,
-    "windDirection": number,
-    "windGust": number,
-    "windSpeed": number
-}
-
-interface DataDaily {
-    "cloudBaseAvg": number,
-    "cloudBaseMax": number,
-    "cloudBaseMin": number,
-    "cloudCeilingAvg": number,
-    "cloudCeilingMax": number,
-    "cloudCeilingMin": number,
-    "cloudCoverAvg": number,
-    "cloudCoverMax": number,
-    "cloudCoverMin": number,
-    "dewPointAvg": number,
-    "dewPointMax": number,
-    "dewPointMin": number,
-    "evapotranspirationAvg": number,
-    "evapotranspirationMax": number,
-    "evapotranspirationMin": number,
-    "evapotranspirationSum": number,
-    "freezingRainIntensityAvg": number,
-    "freezingRainIntensityMax": number,
-    "freezingRainIntensityMin": number,
-    "humidityAvg": number,
-    "humidityMax": number,
-    "humidityMin": number,
-    "iceAccumulationAvg": number,
-    "iceAccumulationLweAvg": number,
-    "iceAccumulationLweMax": number,
-    "iceAccumulationLweMin": number,
-    "iceAccumulationLweSum": number,
-    "iceAccumulationMax": number,
-    "iceAccumulationMin": number,
-    "iceAccumulationSum": number,
-    "moonriseTime": string,
-    "moonsetTime": string,
-    "precipitationProbabilityAvg": number,
-    "precipitationProbabilityMax": number,
-    "precipitationProbabilityMin": number,
-    "pressureSurfaceLevelAvg": number,
-    "pressureSurfaceLevelMax": number,
-    "pressureSurfaceLevelMin": number,
-    "rainAccumulationAvg": number,
-    "rainAccumulationLweAvg": number,
-    "rainAccumulationLweMax": number,
-    "rainAccumulationLweMin": number,
-    "rainAccumulationMax": number,
-    "rainAccumulationMin": number,
-    "rainAccumulationSum": number,
-    "rainIntensityAvg": number,
-    "rainIntensityMax": number,
-    "rainIntensityMin": number,
-    "sleetAccumulationAvg": number,
-    "sleetAccumulationLweAvg": number,
-    "sleetAccumulationLweMax": number,
-    "sleetAccumulationLweMin": number,
-    "sleetAccumulationLweSum": number,
-    "sleetAccumulationMax":number,
-    "sleetAccumulationMin": number,
-    "sleetIntensityAvg": number,
-    "sleetIntensityMax": number,
-    "sleetIntensityMin": number,
-    "snowAccumulationAvg": number,
-    "snowAccumulationLweAvg": number,
-    "snowAccumulationLweMax": number,
-    "snowAccumulationLweMin": number,
-    "snowAccumulationLweSum": number,
-    "snowAccumulationMax": number,
-    "snowAccumulationMin": number,
-    "snowAccumulationSum": number,
-    "snowDepthAvg": number,
-    "snowDepthMax": number,
-    "snowDepthMin": number,
-    "snowDepthSum": number,
-    "snowIntensityAvg": number,
-    "snowIntensityMax": number,
-    "snowIntensityMin": number,
-    "sunriseTime": string,
-    "sunsetTime": string,
-    "temperatureApparentAvg": number,
-    "temperatureApparentMax": number,
-    "temperatureApparentMin": number,
-    "temperatureAvg": number,
-    "temperatureMax": number,
-    "temperatureMin": number,
-    "uvHealthConcernAvg": number,
-    "uvHealthConcernMax": number,
-    "uvHealthConcernMin": number,
-    "uvIndexAvg": number,
-    "uvIndexMax": number,
-    "uvIndexMin": number,
-    "visibilityAvg": number,
-    "visibilityMax": number,
-    "visibilityMin": number,
-    "weatherCodeMax": number,
-    "weatherCodeMin": number,
-    "windDirectionAvg": number,
-    "windGustAvg": number,
-    "windGustMax": number,
-    "windGustMin": number,
-    "windSpeedAvg": number,
-    "windSpeedMax": number,
-    "windSpeedMin": number
-}
-
-interface DataMinutely {
-    time: string,
-    values: ValuesMinutely
-}
-
-interface Location {
-    lat: number,
-    lon: number
-}
-
-const getAllWeather = (): Weather => jsonfile.readFileSync(pathFile);
-
+const probarFuncion = getHourlyTemperatureAndHumidity();
+console.table(probarFuncion);
